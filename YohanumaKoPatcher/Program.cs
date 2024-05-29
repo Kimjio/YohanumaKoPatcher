64845c924b2f3c3abce6c8fbcb9680db66ed74a9
hnststdt <hnststdt@gmail.com>
2024-05-29 14:10:04 +0900

    Pause before exit

--- YohanumaKoPatcher/Program.cs
+++ YohanumaKoPatcher/Program.cs
@@ -11,6 +11,7 @@ var gamePath = Path.GetFullPath(args[0]);
 if (!File.Exists(Path.Join(gamePath, "GameAssembly.dll")))
 {
     Console.WriteLine("Cannot find game data");
+    WaitInput();
     return;
 }
 
@@ -18,6 +19,7 @@ var resourcePath = Path.Join(AppContext.BaseDirectory, "ko-patch-assets");
 if (!Directory.Exists(resourcePath))
 {
     Console.WriteLine("Cannot find patch data");
+    WaitInput();
     return;
 }
 
@@ -37,16 +39,35 @@ using (var sha1 = SHA1.Create())
 if (!versionMap.ContainsKey(hash))
 {
     Console.WriteLine("Unknown game version");
+    WaitInput();
+    return;
+}
+try
+{
+    var patcher = new PatchWorks(gamePath, resourcePath, versionMap[hash]);
+    patcher.PatchBinary();
+    patcher.PatchLanguageDropdown();
+    patcher.PatchFontFallback();
+    patcher.UpdateUnityLocaleTable();
+    patcher.UpdateMasterDataTable();
+    patcher.PatchCatalog();
+    patcher.PatchCatalogSettings();
+    patcher.CopyBundles();
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Patch failed: {e}");
+    WaitInput();
     return;
 }
 
-var patcher = new PatchWorks(gamePath, resourcePath, versionMap[hash]);
-patcher.PatchBinary();
-patcher.PatchLanguageDropdown();
-patcher.PatchFontFallback();
-patcher.UpdateUnityLocaleTable();
-patcher.UpdateMasterDataTable();
-patcher.PatchCatalog();
-patcher.PatchCatalogSettings();
-patcher.CopyBundles();
 Console.WriteLine("Done!");
+WaitInput();
+
+static void WaitInput()
+{
+#if !DEBUG
+    Console.WriteLine("Press any key to exit.");
+    Console.ReadKey(true);
+#endif
+}
